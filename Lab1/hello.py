d8b034504a04e13831c46a4ffe591ac1282768ca
Giovanni Goh <gohgiovanni@gmail.com>
2026-09-04 10:51:29 +0800

    3rd Update

--- Lab1/hello.py
+++ Lab1/hello.py
@@ -10,3 +10,10 @@ followers = 100
 print("Username:", username)
 print("Bio:", bio)
 print("Followers:",followers)
+
+followers += 50
+print("Day 1:", followers)
+followers += 50
+print("Day 2:", followers)
+followers += 50
+print("Day 3:", followers)
